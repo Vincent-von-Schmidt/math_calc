523684381e5df65b83a7d0e7666626877fc40654
Vincent-von-Schmidt <vincentvonschmidt@gmail.com>
2024-02-19 12:08:31 +0100

    lang correction

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ mod token;
 
 fn main() {
     let text: String = "1+2/3".to_string();
-    let lex: Vec<(crate::token::tocken::Operation, usize)> =
-        crate::token::tocken::gen_tockens(&text);
+    let lex: Vec<(crate::token::token::Operation, usize)> = crate::token::token::gen_tockens(&text);
     println!("lex = {:?}", lex);
 }
--- src/token.rs
+++ src/token.rs
@@ -1,4 +1,3 @@
-
 pub mod token {
 
     use std::usize;
