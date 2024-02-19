130c7385cb758ec99218b278e35656dc542f01c1
Vincent-von-Schmidt <vincentvonschmidt@gmail.com>
2024-02-19 11:54:00 +0100

    added first lexer

--- src/token.rs
+++ src/token.rs
@@ -1,4 +1,6 @@
 mod tocken {
+    use std::usize;
+
     // math operations
     pub enum Operation {
         PLUS,
@@ -6,5 +8,23 @@ mod tocken {
         TIMES,
         DIVIDE,
         POWER,
+    }
+
+    /// generates a vector of operation and index
+    ///
+    /// * `text` - text to check
+    pub fn gen_tockens(text: &String) -> Vec<(Operation, usize)> {
+        let mut output: Vec<(Operation, usize)> = Vec::new();
+        for (index, chr) in text.chars().enumerate() {
+            match chr {
+                '+' => output.push((Operation::PLUS, index)),
+                '-' => output.push((Operation::MINUS, index)),
+                '*' => output.push((Operation::TIMES, index)),
+                '/' => output.push((Operation::DIVIDE, index)),
+                '^' => output.push((Operation::POWER, index)),
+                _ => continue,
+            }
+        }
+        return output;
     }
 }
